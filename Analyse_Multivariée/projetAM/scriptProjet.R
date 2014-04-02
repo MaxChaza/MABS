47870be82da6750971a496e3ad1d1272b0d33da8
MaxChaza <maxime.chazalviel@hotmail.fr>
2014-04-02 23:51:34 +0200

    projet Analyse_Multivariée création du jeu de données

---
+++ Analyse_Multivariée/projetAM/scriptProjet.R
@@ -0,0 +1,70 @@
+install.packages("ade4");
+library(ade4);
+tabsp=read.table("tabsp.txt", header=TRUE);
+tabtraits=read.table("tabtraits.txt", header=TRUE);
+tabmil=read.table("tabmil.txt", header=TRUE);
+nom=read.table("noms.txt", header=TRUE);
+altitude=read.table("altitude.txt", header=TRUE);
+
+tabmil4=tabmil;
+tabmil2=tabmil[,2:14];
+tabmil=cbind(tabmil2, altitude);
+
+cca1 = cca(tabsp,tabmil);
+names(cca1);
+s.corcircle(cca1$as);
+s.label(cca1$l1, clab = 0, cpoi = 1.5);
+s.label(cca1$co, add.plot=TRUE);
+
+s.arrow(cca1$fa);
+s.match(cca1$li, cca1$ls, clab=0.5);
+
+install.packages("vegan");
+library(vegan);
+cca2=cca(tabsp, tabmil); 
+plot(cca2);
+cca3=cca(tabsp~Alti + phot + roch,tabmil );
+plot(cca3);
+anova(cca3);
+
+cca4=cca(tabsp~Alti + phot,tabmil );
+plot(cca4);
+anova(cca4);
+
+cca5=cca(tabsp~Alti + phot + arbrp,tabmil );
+plot(cca5);
+anova(cca5);
+
+
+anova(cca(tabsp~herb + Condition(Alti + roch),tabmil ))
+
+#########################################
+tabmil
+altitude
+tabtraits
+
+# Table Présence espèce -> Milieu
+tabsp
+
+# Table Milieu -> Altitude
+altitude
+
+# Table Espèces -> Taille
+tabtraits[,15:19]
+tabEspSize = tabtraits[,15:19];
+tabEspSize
+
+# Table Taille -> Milieu
+tabMilSize = matrix(0, nrow=376, ncol=5) ;
+colnames(tabMilSize) <- colnames(tabEspSize)
+rownames(tabMilSize) <- rownames(tabAlt)
+tabMilSize
+
+for (i in 1:376) {
+  for(j in 1:98){
+    if(tabsp[i,j]==1){
+      
+      tabMilSize[i,]+=1;
+    }
+  }
+}
